3267d12ac547d3565f9f7e13d787a380a8377217
Jon Patrick Arcilla <jp@Jons-MacBook-Pro.local>
2020-10-04 22:10:04 +0900

    first commit

---
+++ 2-JS-basics/final/script_jp.js
@@ -0,0 +1,13 @@
+//console.log("JP's Script");
+
+// Data types
+var firstName = 'jon';
+var middleName;
+var age = 30;
+var job = false;
+
+//console.log(typeof firstName);    string 
+//console.log(typeof middleName);   undefined
+//console.log(typeof age);          number
+//console.log(typeof job);          boolean
+
