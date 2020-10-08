2ac505139b84cd895b36bbd16c53660a74c908c2
jparcilla <jepe.arcilla@gmail.com>
2020-10-09 01:09:52 +0900

    coding challenge #1 complete

--- 2-JS-basics/final/script_jp.js
+++ 2-JS-basics/final/script_jp.js
@@ -1,13 +1,67 @@
 //console.log("JP's Script");
 
-// Data types
+/*
+*** VARIABLE CREATION  ***
+
 var firstName = 'jon';
 var middleName;
 var age = 30;
 var job = false;
 
-//console.log(typeof firstName);    string 
-//console.log(typeof middleName);   undefined
-//console.log(typeof age);          number
-//console.log(typeof job);          boolean
+*** TYPES OF VARIABLES ***
+
+console.log(typeof firstName);    string 
+console.log(typeof middleName);   undefined
+console.log(typeof age);          number
+console.log(typeof job);          boolean
+
+
+
+*** VARIABLE MUTATION AND COERCION
+
+let name = 'John';
+let age = 28;
+let type = name + age;
+
+console.log (name + ' ' + age); // will convert age into string in this command aka COERCION.
+console.log(typeof type); // Output: string + age === string.
+
+let job, isMarried, boolAndStringType;
+job = 'IT Engineer';
+isMarried = prompt('Are you married?'); //prompting: asking users value from the browser.
+boolAndStringType = job + isMarried;
+
+console.log('Job: ' + job);
+console.log('Marital Status : ' + isMarried);
+console.log(typeof boolAndStringType); // Output: string + boolean === string
+
+
+*** MATH OPERATIONS
+
+let x, y;
+x = y = 26; //assign 26 to both x and y. Precedence: y = 26, x = y;
+
+*** CODING CHALLENGE #1
+
+- Create a BMI calculator for Mark and John, and display whose BMI is higher
+
+let Mark, John;
+Mark = new calculateBMI();
+John = new calculateBMI();
+
+function calculateBMI() {
+    mass = prompt('What is your current mass? (Kilograms)');
+    height = prompt('What is your current height? (Meters)');
+    this.mass = mass;
+    this.height = height;
+    this.bmi = mass / (height **=2);
+}
+
+console.log("Mark's BMI is: " + Mark.bmi);
+console.log("John's BMI is: " + John.bmi);
+
+Mark.bmi > John.bmi ? console.log("Mark's BMI is greater: " + Mark.bmi) : console.log("John's BMI is greater: " + John.bmi) ;
+
+*/
+
 
